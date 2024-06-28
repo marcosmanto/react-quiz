dd1d2805c7d5546043fd55c5924c509518b75513
Marcos Mantovani <mantovani.marcos@gmail.com>
2024-06-28 00:59:58 -0300

    display different UIs according with App `Status` defined by the reducer. Initially displays `Loader`, if there was an error to get data display `Error` and if data is received successfully display `StartScreen` component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from './Header'
 import Main from './Main'
+import Loader from './Loader'
+import Error from './Error'
 import { useEffect, useReducer } from 'react'
+import StartScreen from './StartScreen'
 
 const Status = Object.freeze({
   LOADING: Symbol('loading'),
@@ -39,7 +42,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+
+  const numQuestions = questions.length
+
   useEffect(function () {
     fetch('http://localhost:8000/questions')
       .then(res => res.json())
@@ -50,8 +56,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === Status.LOADING && <Loader />}
+        {status === Status.ERROR && <Error />}
+        {status === Status.READY && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   )
