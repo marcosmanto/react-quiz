2827f24df991a945ef03ee585f17929f9236f44c
Marcos Mantovani <mantovani.marcos@gmail.com>
2024-06-28 00:27:20 -0300

    create reducer and `state` with questions and status properties. create two actions to dispatch when fetching data from API: `DATA_RECEIVED` and `DATA_FAILED`. create Status enum to define `state` status prop.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import Header from './Header'
 import Main from './Main'
+import { useEffect, useReducer } from 'react'
+
+const Status = Object.freeze({
+  LOADING: Symbol('loading'),
+  ERROR: Symbol('error'),
+  READY: Symbol('ready'),
+  ACTIVE: Symbol('active'),
+  FINISHED: Symbol('finished')
+})
+
+const Actions = Object.freeze({
+  DATA_RECEIVED: Symbol('datareceived'),
+  DATA_FAILED: Symbol('datafailed')
+})
+
+const initialState = {
+  questions: [],
+  status: Status.LOADING
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case Actions.DATA_RECEIVED:
+      return {
+        ...state,
+        questions: action.payload,
+        status: Status.READY
+      }
+    case Actions.DATA_FAILED:
+      return {
+        ...state,
+        status: Status.ERROR
+      }
+    default:
+      throw new Error('Action unknown')
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: Actions.DATA_RECEIVED, payload: data }))
+      .catch(err => dispatch({ type: Actions.DATA_FAILED }))
+  }, [])
   return (
     <div className="app">
       <Header />
