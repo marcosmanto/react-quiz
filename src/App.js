b22ba69a19b021c79e3470b51b9a31eccec94591
Marcos Mantovani <mantovani.marcos@gmail.com>
2024-06-28 22:13:48 -0300

    start a new quiz

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './Header'
 import Main from './Main'
 import Loader from './Loader'
 import Error from './Error'
+import Question from './Question'
 import { useEffect, useReducer } from 'react'
 import StartScreen from './StartScreen'
 
@@ -15,7 +16,8 @@ const Status = Object.freeze({
 
 const Actions = Object.freeze({
   DATA_RECEIVED: Symbol('datareceived'),
-  DATA_FAILED: Symbol('datafailed')
+  DATA_FAILED: Symbol('datafailed'),
+  START: Symbol('start')
 })
 
 const initialState = {
@@ -36,6 +38,8 @@ function reducer(state, action) {
         ...state,
         status: Status.ERROR
       }
+    case Actions.START:
+      return { ...state, status: Status.ACTIVE }
     default:
       throw new Error('Action unknown')
   }
@@ -58,7 +62,8 @@ export default function App() {
       <Main>
         {status === Status.LOADING && <Loader />}
         {status === Status.ERROR && <Error />}
-        {status === Status.READY && <StartScreen numQuestions={numQuestions} />}
+        {status === Status.READY && <StartScreen numQuestions={numQuestions} dispatch={dispatch} actions={Actions} />}
+        {status === Status.ACTIVE && <Question />}
       </Main>
     </div>
   )
