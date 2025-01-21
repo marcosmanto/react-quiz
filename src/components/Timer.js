1d4bfde9629b0b888c2f5e10969d75be898afb2a
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-21 19:10:37 -0300

    Add useEffect needed dependency in `Timer` component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,13 +4,16 @@ function Timer({ dispatch, secondsRemaining }) {
   const mins = String(Math.floor(secondsRemaining / 60))
   const seconds = String(secondsRemaining % 60)
 
-  useEffect(function () {
-    const id = setInterval(() => {
-      dispatch({ type: 'tick' })
-    }, 1000)
+  useEffect(
+    function () {
+      const id = setInterval(() => {
+        dispatch({ type: 'tick' })
+      }, 1000)
 
-    return () => clearInterval(id)
-  }, [])
+      return () => clearInterval(id)
+    },
+    [dispatch]
+  )
   return (
     <div className="timer">
       {mins.padStart(2, '0')}:{seconds.padStart(2, '0')}
